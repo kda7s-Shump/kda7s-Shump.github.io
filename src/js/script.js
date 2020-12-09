c290df3a586e4eb4e2b739aca9c085a0a3c4eeb2
kda-7sShump <63225006+kda7s-Shump@users.noreply.github.com>
2020-12-09 13:46:50 +0900

    未完成ながら公開

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,18 @@
-$(function () {
-  $("h1").css("color", "blue");
-});
+function imgPreview(event){
+  var imgfile = event.target.files[0];
+  var reader = new FileReader();
+  var preview = document.getElementById('preview');
+  var previewImage = document.getElementById("previewImage");
+
+  if (previewImage != null) {
+    preview.removeChild(previewImage);
+  }
+  reader.onload = function (event) {
+    var img = document.createElement("img");
+    img.setAttribute("src", reader.result);
+    img.setAttribute("id", "previewImage");
+    img.setAttribute("style", "width:100%;");
+    preview.appendChild(img);
+  };
+  reader.readAsDataURL(imgfile);
+}
